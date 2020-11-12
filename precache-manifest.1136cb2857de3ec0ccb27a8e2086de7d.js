4fc1e92ed38d40ec7628b1a930b62b997d495bf9
Khurshid Ali <khurshidweb@yahoo.com>
2020-11-12 19:27:38 +0530

    Updates

---
+++ precache-manifest.1136cb2857de3ec0ccb27a8e2086de7d.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "67cf276afddeca3984296e5930ca8682",
+    "url": "/reactsite/index.html"
+  },
+  {
+    "revision": "f8381947c7f7383ef037",
+    "url": "/reactsite/static/css/2.29e03083.chunk.css"
+  },
+  {
+    "revision": "5ab9f86d72efff1ec776",
+    "url": "/reactsite/static/css/main.05686f4e.chunk.css"
+  },
+  {
+    "revision": "f8381947c7f7383ef037",
+    "url": "/reactsite/static/js/2.b2085db4.chunk.js"
+  },
+  {
+    "revision": "e92311d493a1dde02c3c18735aca81e5",
+    "url": "/reactsite/static/js/2.b2085db4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5ab9f86d72efff1ec776",
+    "url": "/reactsite/static/js/main.a22dc8be.chunk.js"
+  },
+  {
+    "revision": "5dd80a3f7cd010e1b662",
+    "url": "/reactsite/static/js/runtime-main.bd39602f.js"
+  },
+  {
+    "revision": "74191e6406c33104241e3c572bedb53e",
+    "url": "/reactsite/static/media/1.74191e64.jpg"
+  },
+  {
+    "revision": "b095867b9af90ef84efdb125a730ed8f",
+    "url": "/reactsite/static/media/2.b095867b.jpg"
+  },
+  {
+    "revision": "51c4d00d87f24c060e1929b5fd817b70",
+    "url": "/reactsite/static/media/3.51c4d00d.jpg"
+  },
+  {
+    "revision": "90382bcfbbdbe119cee59732d0efbd7e",
+    "url": "/reactsite/static/media/4.90382bcf.jpg"
+  },
+  {
+    "revision": "5a09b8e9c6d7d1ac0c8534e23e5a77fe",
+    "url": "/reactsite/static/media/5.5a09b8e9.jpg"
+  },
+  {
+    "revision": "29782150099696661ab6a7b69321013a",
+    "url": "/reactsite/static/media/6.29782150.jpg"
+  },
+  {
+    "revision": "c539af336d0d3d204133db092c080936",
+    "url": "/reactsite/static/media/Khurshid-CV.c539af33.docx"
+  },
+  {
+    "revision": "2bba19e9cf4363e7fc7da996244d8521",
+    "url": "/reactsite/static/media/about-me.2bba19e9.png"
+  },
+  {
+    "revision": "5e343410bf049574b90d7e534ef35261",
+    "url": "/reactsite/static/media/bg1.5e343410.jpg"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/reactsite/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/reactsite/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/reactsite/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/reactsite/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/reactsite/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "c6df25ebd061925e2170019a755f9222",
+    "url": "/reactsite/static/media/photo.c6df25eb.png"
+  }
+]);
